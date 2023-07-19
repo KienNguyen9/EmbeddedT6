6bd24dfb138798f5fa0e8261e310869bcbfb953f
KienNguyen9 <kierannguyen98@gmail.com>
2023-07-19 23:49:24 +0700

    Update file

--- Lesson_05_Pointer/Example-2-Pointer-to-pointer/main.c
+++ Lesson_05_Pointer/Example-2-Pointer-to-pointer/main.c
@@ -1,1 +1,34 @@
-// test
+/*
+* File: main.c
+* Author: Kien Nguyen
+* Date: 19/06/2023
+* Description: This is a sample file for pointer
+*/
+
+#include <stdio.h>
+ 
+int main()
+{
+
+    int a = 10;
+    int *ptr = &a;
+    int **ptp =&ptr;
+
+    printf("Dia chi cua con tro ptr: %p, gia tri = %d\n", ptp, **ptp);
+
+
+    int var = 123;
+ 
+    int* ptr2;
+ 
+    int** ptr1;
+ 
+    ptr2 = &var;
+ 
+    ptr1 = &ptr2;
+ 
+    printf("Gia tri var = %d\n", var);
+    printf("Gia tri *pointer = %d\n", *ptr2);
+    printf("Gia tri **pointer = %d\n", **ptr1);
+    return 0;
+}
