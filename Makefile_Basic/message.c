a755a590fe5e59e795d64abe91c9f8879706170b
KienNguyen9 <kierannguyen98@gmail.com>
2023-07-22 15:47:34 +0700

    Update file

---
+++ Makefile_Basic/message.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+// This is the message.c file
+void test(){
+    printf("test");
+};
---
+++ Makefile_Basic/message.h
@@ -0,0 +1,7 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+// This is the message
+const char *message = "Makefile Basic";
+
+#endif // MESSAGE_H
